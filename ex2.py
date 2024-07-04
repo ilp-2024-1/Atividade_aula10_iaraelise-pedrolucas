e13a12776bac0c44f732fb26cbbe99b04ee2397c
Pedro Lucas Pereira de Medeiros <20241148060023@ifrn.local>
2024-07-04 09:26:28 -0300

    ex2.py

---
+++ ex2.py
@@ -0,0 +1,13 @@
+valor = 1
+soma = 0
+
+print("Digite valores: ")
+while valor != 0:
+ valor = int(input())
+ valor != 0
+ soma = soma + valor
+
+if valor == 0:
+  print ("Fim do programa", soma)
+
+
