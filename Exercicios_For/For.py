eedc23702a9c078afb187194e4fb14f4a934a06c
Pedro Lucas Pereira de Medeiros <20241148060023@ifrn.local>
2024-07-10 11:56:25 -0300

    For.py

---
+++ Exercicios_For/For.py
@@ -0,0 +1,11 @@
+#Questão 1 
+# for i in range (2, 13, 2):
+#     print (i, end=",")
+# print ('...')
+
+#Questão 2
+# numero = int(input("Digite um número: "))
+
+# for i in range (1, numero + 1):
+#     print (i, end=",") 
+# print ("Fim.")
