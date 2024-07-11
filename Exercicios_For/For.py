bcfb7d5b08032e7b6e99d08c822f510311c54b26
Pedro Lucas Pereira de Medeiros <20241148060023@ifrn.local>
2024-07-11 07:35:29 -0300

    For.py

--- Exercicios_For/For.py
+++ Exercicios_For/For.py
@@ -8,3 +8,19 @@
 # for i in range (1, numero + 1):
 #     print (i, end=",") 
 # print ("Fim.")
+
+#Questão 3
+# v_ini = int(input("Insira um valor positivo: "))
+# v_fim = int(input("Insira um segundo valor positivo: "))
+
+# for i in range (v_ini, v_fim+1):
+#     print (i, end=(","))
+# print ("\n Fim do programa!")
+
+#Questão 4 
+v_ini = int(input("Insira um valor positivo: "))
+v_fim = int(input("Insira outro valor positivo: "))
+
+for i in range (v_ini+1, v_fim):
+    print (i, end=(","))
+print ("\n Fim do programa!")
